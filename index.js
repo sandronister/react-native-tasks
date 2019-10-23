d44958df4bea1d035a55a7c7e9c5caaf1f3c8128
sandroni <sandronister@gmail.com>
2019-10-23 16:01:20 -0300

    Notebook component

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Notebook from './src/screens/Notebook'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Notebook);
