6879cfa862282cb4d0906d9dd5e4d6440cf46a13
julio <sandronister@gmail.com>
2019-10-26 10:22:01 -0300

    flatList and task component

--- src/screens/Notebook.js
+++ src/screens/Notebook.js
@@ -1,60 +1,124 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ImageBackground, FlatList } from 'react-native';
 import moment from 'moment';
 import 'moment/locale/pt-br';
 import todayImage from '../../assets/imgs/today.jpg';
 import commonStyles from '../commonStyle';
+import Task from '../components/Task';
 
 export default class Notebook extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <ImageBackground source={todayImage} style={styles.background}>
-          <View style={styles.titleBar}>
-            <Text style={styles.title}>Hoje</Text>
-            <Text style={styles.subtitle}>
-              {moment()
-                .locale('pt-br')
-                .format('ddd, D [de] MMMM')}
-            </Text>
-          </View>
-        </ImageBackground>
-        <View style={styles.taskContainer}>
-          <Text>Tarefa 1</Text>
-          <Text>Tarefa 2</Text>
-          <Text>Tarefa 3</Text>
-        </View>
-      </View>
-    );
-  }
+	state = {
+		task: [
+			{
+				id: Math.random(),
+				desc: 'Aprender React-Native',
+				estimateAt: new Date(),
+				doneAt: new Date()
+			},
+			{
+				id: Math.random(),
+				desc: 'Aprender Amazon Lambda',
+				estimateAt: new Date(),
+				doneAt: null
+			},
+			{
+				id: Math.random(),
+				desc: 'Aprender React-Native',
+				estimateAt: new Date(),
+				doneAt: new Date()
+			},
+			{
+				id: Math.random(),
+				desc: 'Aprender Amazon Lambda',
+				estimateAt: new Date(),
+				doneAt: null
+			},
+			{
+				id: Math.random(),
+				desc: 'Aprender React-Native',
+				estimateAt: new Date(),
+				doneAt: new Date()
+			},
+			{
+				id: Math.random(),
+				desc: 'Aprender Amazon Lambda',
+				estimateAt: new Date(),
+				doneAt: null
+			},
+			{
+				id: Math.random(),
+				desc: 'Aprender React-Native',
+				estimateAt: new Date(),
+				doneAt: new Date()
+			},
+			{
+				id: Math.random(),
+				desc: 'Aprender Amazon Lambda',
+				estimateAt: new Date(),
+				doneAt: null
+			},
+			{
+				id: Math.random(),
+				desc: 'Aprender React-Native',
+				estimateAt: new Date(),
+				doneAt: new Date()
+			},
+			{
+				id: Math.random(),
+				desc: 'Aprender Amazon Lambda',
+				estimateAt: new Date(),
+				doneAt: null
+			}
+		]
+	};
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<ImageBackground source={todayImage} style={styles.background}>
+					<View style={styles.titleBar}>
+						<Text style={styles.title}>Hoje</Text>
+						<Text style={styles.subtitle}>{moment().locale('pt-br').format('ddd, D [de] MMMM')}</Text>
+					</View>
+				</ImageBackground>
+				<View style={styles.taskContainer}>
+					<FlatList
+						data={this.state.task}
+						keyExtractor={(item) => `${item.id}`}
+						renderItem={({ item }) => <Task {...item} />}
+					/>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  background: {
-    flex: 3,
-  },
-  titleBar: {
-    flex: 1,
-    justifyContent: 'flex-end',
-  },
-  title: {
-    fontFamily: commonStyles.fontFamily,
-    color: commonStyles.colors.secondary,
-    fontSize: 50,
-    marginLeft: 20,
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontFamily: commonStyles.fontFamily,
-    color: commonStyles.colors.secondary,
-    fontSize: 20,
-    marginLeft: 20,
-    marginBottom: 30,
-  },
-  taskContainer: {
-    flex: 7,
-  },
+	container: {
+		flex: 1
+	},
+	background: {
+		flex: 3
+	},
+	titleBar: {
+		flex: 1,
+		justifyContent: 'flex-end'
+	},
+	title: {
+		fontFamily: commonStyles.fontFamily,
+		color: commonStyles.colors.secondary,
+		fontSize: 50,
+		marginLeft: 20,
+		marginBottom: 10
+	},
+	subtitle: {
+		fontFamily: commonStyles.fontFamily,
+		color: commonStyles.colors.secondary,
+		fontSize: 20,
+		marginLeft: 20,
+		marginBottom: 30
+	},
+	taskContainer: {
+		flex: 7
+	}
 });
